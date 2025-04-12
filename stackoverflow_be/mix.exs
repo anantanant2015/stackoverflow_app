18f788f31466c9efda4ee3f44f7b0d0ffe6dca12
Anant <anantanant2015@gmail.com>
2025-04-12 19:50:22 +0530

    Adds CORS, SSL, Updates envs

--- stackoverflow_be/mix.exs
+++ stackoverflow_be/mix.exs
@@ -55,12 +55,12 @@ defmodule StackoverflowBe.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:httpoison, "~> 1.8"},
       {:jason, "~> 1.2"},
-      {:openai, "~> 0.2.0"}
+      {:openai, "~> 0.2.0"},
+      {:cors_plug, "~> 3.0"}
     ]
   end
 
