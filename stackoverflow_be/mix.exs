3b165a088c9931e2b2c26c1b16b5aac14652d101
Anant <anantanant2015@gmail.com>
2025-04-15 18:38:15 +0530

    Fixes docker missing pieces and other issues

--- stackoverflow_be/mix.exs
+++ stackoverflow_be/mix.exs
@@ -55,7 +55,6 @@ defmodule StackoverflowBe.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:cors_plug, "~> 3.0"},
