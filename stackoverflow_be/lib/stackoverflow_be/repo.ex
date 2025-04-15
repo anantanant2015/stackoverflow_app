4aaf23970a254ecd631557d22d7db838c85fdc5d
Anant <anantanant2015@gmail.com>
2025-04-15 16:15:25 +0530

    Revert "Fe updates"

--- stackoverflow_be/lib/stackoverflow_be/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule StackoverflowBe.Repo do
-  use Ecto.Repo,
-    otp_app: :stackoverflow_be,
-    adapter: Ecto.Adapters.Postgres
-end
--- stackoverflow_fe/src/components/common/LoginButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useAuth } from '../auth/useAuth';
-
-export const LoginButton = () => {
-  const { login } = useAuth();
-  return <button onClick={login}>Login with StackExchange</button>;
-};
